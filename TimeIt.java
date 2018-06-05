98a8040e7e295ef505aa34f0b4895e86cfbd5592
zjevander <zjevander@gmail.com>
2018-06-05 01:06:05 -0400

    Changed formatter to display appropriate leading zeroes in seconds column

--- TimeIt.java
+++ TimeIt.java
@@ -5,15 +5,15 @@ public class TimeIt {
             block.run();
         } finally {
             long end = System.nanoTime();
-            System.out.printf("Time taken(s): %s\n", convertNanosToFormattedTime((end - start) / 1_000_000.0));
+            System.out.printf("Time taken(s): %s\n", convertNanosToFormattedTime((end - start) / 1_000_000));
         }
     }
 
-    private static String convertNanosToFormattedTime(double millis){
-        double seconds = (millis / 1000.0) % 60;
+    private static String convertNanosToFormattedTime(long millis){
+        long seconds = (long)(millis / 1000.0) % 60;
         long minutes = (long)(millis / (1000 * 60)) % 60;
         long hours = (long)(millis / (1000 * 60 * 60)) % 24;
 
-        return String.format("%02d:%02d:%02.3f", hours, minutes, seconds);
+        return String.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, millis % 1000);
     }
 }
